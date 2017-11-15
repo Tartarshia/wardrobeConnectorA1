99a110180a7b1f03fde1b2c4a59cc730ed002bc1
Tatasha <tartarshia@gmail.com>
2017-11-14 23:47:39 -0500

    saving 11/14/2017

---
+++ main.js
@@ -0,0 +1,31 @@
+var mainApp = angular.module('mainApp', []);
+
+mainApp.controller('mainCtrl', ['$scope', function($scope) {
+        var test_array =
+        [  ['Legend of the Kid','Hair','1112','5','A','','S','','SS','','SS','','','A','','event','Zashiki','150'],
+          ['Guardian Kami','Dress','921','5','A','','S','','SS','','SS','','','A','','event','Zashiki','150'],
+          ['Strange Neighbor','Dress','922','5','A','','S','','SS','','SS','','','A','Kimono','event','Zashiki','150'],
+          ['Samsara Twilight','Coat','453','5','A','','S','','SS','','SS','','','A','','event','Zashiki','150'],
+          ['Samsara Twilight','Hosiery-Socks','426','5','A','','S','','SS','','SS','','','A','','event','Zashiki','150'],
+          ['Samsara Twilight','Shoes','993','5','A','','S','','SS','','SS','','','A','','event','Zashiki','150'],
+          ['Story in Memory','Accessory-Headwear·Hair ornaments','2062','5','A','','S','','SS','','SS','','','A','','event','Zashiki','150'],
+          ['Handbag in Childhood','Accessory-Handheld·Right','2063','5','A','','S','','SS','','SS','','','A','','event','Zashiki','150'],
+          ['Kid`s Doll','Accessory-Handheld·Left','2064','5','A','','S','','SS','','SS','','','A','','event','Zashiki','150']];
+            var fname = "IJGResults";
+        
+            var csvContent = "data:text/csv;charset=utf-8,";
+            
+console.info(test_array);
+$scope.test_array = test_array;
+        $scope.press = function() {
+            test_array.forEach(function(infoArray, index){
+                dataString = infoArray.join(",");
+                console.log(dataString);
+                csvContent += dataString+ "\n";
+            });
+    
+            var encodedUri = encodeURI(csvContent);
+            window.open(encodedUri);
+        };
+    }]);
+
