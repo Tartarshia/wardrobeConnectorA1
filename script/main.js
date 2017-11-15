060c311b45f3cefa1cf5e06aa0352b8b459653a1
Tatasha <tartarshia@gmail.com>
2017-11-15 09:01:47 -0500

    try to display item in array by detail

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 var mainApp = angular.module('mainApp', []);
 
 mainApp.controller('mainCtrl', ['$scope', function($scope) {
-        var raw_array = [
+        $scope.raw_array = [
             ['Legend of the Kid','Hair','1112','5','A','','S','','SS','','SS','','','A','','event','Zashiki','150'],
             ['Guardian Kami','Dress','921','5','A','','S','','SS','','SS','','','A','','event','Zashiki','150'],
             ['Strange Neighbor','Dress','922','5','A','','S','','SS','','SS','','','A','Kimono','event','Zashiki','150'],
@@ -12,13 +12,10 @@ mainApp.controller('mainCtrl', ['$scope', function($scope) {
             ['Handbag in Childhood','Accessory-Handheld·Right','2063','5','A','','S','','SS','','SS','','','A','','event','Zashiki','150'],
             ['Kid`s Doll','Accessory-Handheld·Left','2064','5','A','','S','','SS','','SS','','','A','','event','Zashiki','150']
         ];
-            var fname = "IJGResults";
-        
-            var csvContent = "";
 
-$scope.raw_array = raw_array;
 
         $scope.press = function() {
+            var csvContent = "", raw_array = $scope.raw_array;
             raw_array.forEach(function(infoArray, index){
                 dataString = infoArray.join(",");
                 csvContent += dataString+ "\n";
